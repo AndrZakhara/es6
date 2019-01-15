1fcc3d58bab42b35e29dbbbc43fc0ee9ca557c62
Andrii Zakharchenko <art.pruttov@gmail.com>
2019-01-15 16:05:26 +0200

    fix recursion function

--- generator/index.js
+++ generator/index.js
@@ -1,59 +1,100 @@
-const resultArray = [];
-let state = false;
-
 function runner(generator) {
+  const resultArray = [];
 
-    let data = generator.next();
-    let {value, done } = data;
-    console.log(`value: ${value}`);
-    console.log(`done: ${done}`);
+  function execute(generator, yieldValue) {
+    let next = generator.next(yieldValue);
 
-    const executor = (generator, value) => {
-      switch (typeof value) {
-        case 'function':
-          console.log('function');
-          const result = value();
-          resultArray.push(result);
-          data = generator.next(result);
-          value = data.value;
-          done = data.done;
-          break;
-        case 'object':
-          // if(value instanceof Promise) {
-          //   value.resolve(data).then(data => {
-          //     value = data;
-          //     console.log(`value: ${value}`);
-          //   });
-          //   console.log('promise');
-          //   break;
-          // } else {
-          console.log('object');
-          resultArray.push(value);
-          data = generator.next(value);
-          value = data.value;
-          done = data.done;
-          break;
-        // }
-
-        default:
-          console.log('primitive');
-          resultArray.push(value);
-          data = generator.next(value);
-          value = data.value;
-          done = data.done;
+    if (!next.done) {
+      if(typeof next.value === 'function') {
+        const result = next.value();
+        resultArray.push(result);
+        execute(generator, result);
       }
-      if(state === false) {
-        executor(generator, value);
-      } else
-        return;
-    };
+      else if (next.value instanceof Promise) {
+        next.value.then(
+          data => {
+            resultArray.push(data);
+            execute(generator, data);
+          },
+          err => {
+            console.log(err);
+          }
+        );
 
-
-
-
-  console.log(data);
-  // return runner(generator);
+      }
+      else {
+        const result = next.value;
+        resultArray.push(result);
+        execute(generator, result);
+      }
+    } else {
+      return;
+    }
+  }
+  execute(generator);
+  console.log(resultArray);
 }
+
+//     let data = generator.next();
+//     let { value, done } = data;
+//     console.log(`value: ${value}`);
+//     console.log(`done: ${done}`);
+//
+//     const executor = (generator, value) => {
+//       console.log('-----')
+//       resultArray.push(value);
+//       switch (typeof value) {
+//         case 'function':
+//           console.log('function');
+//           const result = value();
+//           resultArray.push(result);
+//           data = generator.next(result);
+//           value = data.value;
+//           done = data.done;
+//           executor(generator, value);
+//           break;
+//         case 'object':
+//           if(value instanceof Promise) {
+//             value.then(data => {
+//               value = data;
+//               console.log(`value: ${value}`);
+//             });
+//             executor(generator, value);
+//             break;
+//           } else {
+//           console.log('object');
+//           resultArray.push(value);
+//           data = generator.next(value);
+//           value = data.value;
+//           done = data.done;
+//           executor(generator, value);
+//           break;
+//         }
+//
+//         default:
+//           console.log('primitive');
+//           resultArray.push(value);
+//           data = generator.next(value);
+//           value = data.value;
+//           done = data.done;
+//           executor(generator, value);
+//       }
+//
+//       if (!done) {
+//         value.then(
+//           result => execute(generator, result),
+//           err => generator.throw(err)
+//         );
+//
+//         return;
+//     };
+//
+//
+//
+//
+//   console.log(data);
+//   // return runner(generator);
+// }
 
 const sum = (a, b) => a + b;
 
@@ -69,7 +110,5 @@ function *gen() {
   const c = yield {user: 'userName'};
   // console.log(a, a1, b, c);
 }
-console.log(resultArray);
 const g = gen();
 runner(g);
-console.log(resultArray);
